48c02f0a7cfce2fdae6756cc0561b21b9c386832
Michael Nien <mobile.michael.nien@gmail.com>
2025-09-01 17:57:02 +0800

    更新 產生 osv scanner report 檔案名稱

--- script/google_osv-scanner.bat
+++ script/google_osv-scanner.bat
@@ -1,10 +1,10 @@
 @echo off
 
-set google-osv-scanner-version=2.2.1
+set google-osv-scanner-version=2.2.2
 echo %google-osv-scanner-version%
 
 ::mkdir
 mkdir ..\sbom\poc\
 
-::module opForeignWorkers
-"..\google_osv-scanner\v%google-osv-scanner-version%\osv-scanner_windows_amd64.exe" --sbom=..\app\build\reports\bom.json --format=table --output=..\sbom\poc\sbom_report.txt
+::module app
+"..\google_osv-scanner\v%google-osv-scanner-version%\osv-scanner_windows_amd64.exe" --sbom=..\app\build\reports\bom.json --format=table --output=..\sbom\poc\sbom_osv_scanner_report.txt
